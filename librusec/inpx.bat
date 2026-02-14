5112599e038e8608398c9d5aaab36d09765addc5
heimdallr <heimdalrnsk@gmail.com>
2026-02-14 17:51:46 +0700

    Нужны вполне конкретные архивы, не все

--- librusec/inpx.bat
+++ librusec/inpx.bat
@@ -1,5 +1,5 @@
 fliparser.exe ^
-D:\media\books\fb2.lib.rus.ec.7z\*.7z;%~dp0hash ^
+D:\media\books\fb2.lib.rus.ec.7z\fb2-*.7z;%~dp0hash_l ^
 -o t:\ ^
 --dump t:\fb2.lib.rus.ec.7z.db ^
 --collection-info-template %~dp0collection.info
