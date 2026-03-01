35ee50008333565ef56d75914f880280a2035975
heimdallr <heimdalrnsk@gmail.com>
2026-03-02 02:31:48 +0700

    hash_f, hash_l больше нет

--- flibusta/inpx.bat
+++ flibusta/inpx.bat
@@ -1,5 +1,5 @@
 fliparser.exe ^
-D:\media\books\fb2.Flibusta.Net.7z\*fb2-*.7z;%~dp0hash_f ^
+D:\media\books\fb2.Flibusta.Net.7z\*fb2-*.7z;%~dp0hash ^
 -o t:\ ^
 --dump t:\fb2.Flibusta.Net.7z.db ^
 --collection-info-template %~dp0collection.info
