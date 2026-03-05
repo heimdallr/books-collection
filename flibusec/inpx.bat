4668f7ffeee8ae45f9a154502461f17e9ba27590
heimdallr <heimdalrnsk@gmail.com>
2026-03-05 15:13:42 +0700

    Дампы укажем точнее. А то лезем в левые базы

--- flibusec/inpx.bat
+++ flibusec/inpx.bat
@@ -1,5 +1,5 @@
 fliparser.exe ^
 D:\media\books\fb2.flibusta.lib.rus.ec.7z\*fb2-*.7z;%~dp0hash ^
 -o t:\ ^
---dump t:\*.db ^
+--dump t:\fb2.*.7z.db ^
 --collection-info-template %~dp0collection.info
