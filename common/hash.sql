09c48f11e679c58ea933a673ceb1a60c463d7173
heimdallr <heimdalrnsk@gmail.com>
2026-09-12 13:31:14 +0700

    Изменения DDL базы хэшей

--- common/hash.sql
+++ common/hash.sql
@@ -2,8 +2,6 @@ PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
 
 -- Таблица: File
-DROP TABLE IF EXISTS File;
-
 CREATE TABLE File (
     FileId      INTEGER         PRIMARY KEY AUTOINCREMENT
                                 NOT NULL,
@@ -26,8 +24,6 @@ CREATE TABLE File (
 
 
 -- Таблица: Folder
-DROP TABLE IF EXISTS Folder;
-
 CREATE TABLE Folder (
     FolderId        INTEGER      PRIMARY KEY AUTOINCREMENT
                                  NOT NULL,
@@ -39,8 +35,6 @@ CREATE TABLE Folder (
 
 
 -- Таблица: Histogram
-DROP TABLE IF EXISTS Histogram;
-
 CREATE TABLE Histogram (
     HistogramId INTEGER      PRIMARY KEY AUTOINCREMENT
                              NOT NULL,
@@ -56,8 +50,6 @@ CREATE TABLE Histogram (
 
 
 -- Таблица: Image
-DROP TABLE IF EXISTS Image;
-
 CREATE TABLE Image (
     ImageId     INTEGER       PRIMARY KEY AUTOINCREMENT
                               NOT NULL,
@@ -79,9 +71,16 @@ CREATE TABLE Image (
 );
 
 
--- Таблица: Section
-DROP TABLE IF EXISTS Section;
+-- Таблица: Replacement
+CREATE TABLE Replacement (
+    Folder       VARCHAR (64),
+    File         VARCHAR (256),
+    FolderOrigin VARCHAR (64),
+    FileOrigin   VARCHAR (256) 
+);
 
+
+-- Таблица: Section
 CREATE TABLE Section (
     SectionId       INTEGER   PRIMARY KEY AUTOINCREMENT
                               NOT NULL,
@@ -100,8 +99,6 @@ CREATE TABLE Section (
 
 
 -- Таблица: SourceLibrary
-DROP TABLE IF EXISTS SourceLibrary;
-
 CREATE TABLE SourceLibrary (
     SourceLibraryId INTEGER      PRIMARY KEY AUTOINCREMENT
                                  NOT NULL,
@@ -112,68 +109,77 @@ CREATE TABLE SourceLibrary (
 INSERT INTO SourceLibrary (Name) VALUES ('flibusta');
 INSERT INTO SourceLibrary (Name) VALUES ('librusec');
 
--- Индекс: IS_File_OriginId
-DROP INDEX IF EXISTS IS_File_OriginId;
-
-CREATE INDEX IS_File_OriginId ON File (
-    OriginId
+-- Индекс: IX_File_Hash
+CREATE INDEX IX_File_Hash ON File (
+    Hash
 );
 
 
 -- Индекс: IX_FK_File_FolderId
-DROP INDEX IF EXISTS IX_FK_File_FolderId;
-
 CREATE INDEX IX_FK_File_FolderId ON File (
     FolderId
 );
 
 
--- Индекс: IX_FK_Histogram_FileId
-DROP INDEX IF EXISTS IX_FK_Histogram_FileId;
+-- Индекс: IX_FK_File_OriginId
+CREATE INDEX IX_FK_File_OriginId ON File (
+    OriginId
+);
 
+
+-- Индекс: IX_FK_Histogram_FileId
 CREATE INDEX IX_FK_Histogram_FileId ON Histogram (
     FileId
 );
 
 
 -- Индекс: IX_FK_Image_FileId
-DROP INDEX IF EXISTS IX_FK_Image_FileId;
-
 CREATE INDEX IX_FK_Image_FileId ON Image (
     FileId
 );
 
 
 -- Индекс: IX_FK_Section_FileId
-DROP INDEX IF EXISTS IX_FK_Section_FileId;
-
 CREATE INDEX IX_FK_Section_FileId ON Section (
     FileId
 );
 
 
--- Индекс: IX_Folder_SourceLibraryId
-DROP INDEX IF EXISTS IX_Folder_SourceLibraryId;
+-- Индекс: IX_FK_Section_ParentSectionId
+CREATE INDEX IX_FK_Section_ParentSectionId ON Section (
+    ParentSectionId
+);
 
+
+-- Индекс: IX_Folder_SourceLibraryId
 CREATE INDEX IX_Folder_SourceLibraryId ON Folder (
     SourceLibraryId
 );
 
 
 -- Индекс: IX_Image_Md5
-DROP INDEX IF EXISTS IX_Image_Md5;
-
 CREATE INDEX IX_Image_Md5 ON Image (
     Md5
 );
 
 
--- Индекс: IX_Section_ParentSectionId
-DROP INDEX IF EXISTS IX_Section_ParentSectionId;
-
-CREATE INDEX IX_Section_ParentSectionId ON Section (
-    ParentSectionId
-);
+-- Представление: CurrentReplacement
+CREATE VIEW CurrentReplacement AS
+    SELECT d.Name Folder,
+           f.Name File,
+           d1.Name FolderOrigin,
+           f1.Name FileOrigin
+      FROM File f
+           JOIN
+           Folder d ON d.FolderId = f.FolderId
+           JOIN
+           File f1 ON f1.FileId = f.OriginId
+           JOIN
+           Folder d1 ON d1.FolderId = f1.FolderId
+     ORDER BY d.Name,
+              f.Name,
+              d1.Name,
+              f1.Name;
 
 
 COMMIT TRANSACTION;
