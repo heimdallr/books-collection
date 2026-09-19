f7e09927b2fe803dba33ff1c86e3787fa512249a
heimdallr <heimdalrnsk@gmail.com>
2026-09-20 01:37:18 +0700

    Поправим-ка Пушкина

--- common/replace.sql
+++ common/replace.sql
@@ -24,6 +24,7 @@ INSERT INTO Author (LibraryId, AuthorId, FirstName, MiddleName, LastName) VALUES
 INSERT INTO Author (LibraryId, AuthorId, FirstName, MiddleName, LastName) VALUES (1, 36085, 'Сергей', NULL, 'Лукьяненко');
 INSERT INTO Author (LibraryId, AuthorId, FirstName, MiddleName, LastName) VALUES (1, 72206, 'Константин', 'Георгиевич', 'Калбазов');
 INSERT INTO Author (LibraryId, AuthorId, FirstName, MiddleName, LastName) VALUES (1, 142441, 'Константин', 'Георгиевич', 'Калбазов');
+INSERT INTO Author (LibraryId, AuthorId, FirstName, MiddleName, LastName) VALUES (1, 321888, 'Александр', 'Сергеевич', 'Пушкин');
 INSERT INTO Author (LibraryId, AuthorId, FirstName, MiddleName, LastName) VALUES (2, 204458, 'Коллектив авторов', '--', 'Составитель С. Лукьяненко');
 INSERT INTO Author (LibraryId, AuthorId, FirstName, MiddleName, LastName) VALUES (2, 316127, 'Патрик', 'Дж.', 'Холл');
 INSERT INTO Author (LibraryId, AuthorId, FirstName, MiddleName, LastName) VALUES (2, 346366, 'Гоблин', NULL, 'MeXXanik');
